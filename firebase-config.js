a9e59df05100d610d59e6439bcb40714c37d3455
aquiadi <aqui.adi.j@gmail.com>
2025-08-08 01:26:59 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,9 @@
 // Firebase configuration
-export const firebaseConfig = {
-    apiKey: "your-api-key-here",
-    authDomain: "your-project-id.firebaseapp.com",
-    projectId: "your-project-id",
-    storageBucket: "your-project-id.appspot.com",
-    messagingSenderId: "your-sender-id",
-    appId: "your-app-id"
+const firebaseConfig = {
+  apiKey: "AIzaSyCX3sz4zgX4NCZ_7be1uRWIVnnyABRwcAc",
+  authDomain: "coming-soon-emails.firebaseapp.com",
+  projectId: "coming-soon-emails",
+  storageBucket: "coming-soon-emails.firebasestorage.app",
+  messagingSenderId: "408306020232",
+  appId: "1:408306020232:web:d1642739e8c38a2605d43d"
 };
