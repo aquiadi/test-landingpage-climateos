a18557a115a9880be046616ce825716e570ff5d9
aquiadi <aqui.adi.j@gmail.com>
2025-08-08 01:31:08 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCX3sz4zgX4NCZ_7be1uRWIVnnyABRwcAc",
-  authDomain: "coming-soon-emails.firebaseapp.com",
-  projectId: "coming-soon-emails",
-  storageBucket: "coming-soon-emails.firebasestorage.app",
-  messagingSenderId: "408306020232",
-  appId: "1:408306020232:web:d1642739e8c38a2605d43d"
-};
